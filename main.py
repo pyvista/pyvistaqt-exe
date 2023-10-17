d5ed2e230148c13a676693a174fce4aa1b90fc6e
Bane Sullivan <banesullivan@gmail.com>
2023-10-16 20:01:52 -0700

    Add more menu controls

--- main.py
+++ main.py
@@ -7,9 +7,9 @@ os.environ["QT_API"] = "pyqt5"
 import numpy as np
 import pyvista as pv
 from pyvistaqt import MainWindow, QtInteractor
-from qtpy import QtGui, QtWidgets
 from pyvistaqt.dialog import ScaleAxesDialog
 from pyvistaqt.utils import _create_menu_bar
+from qtpy import QtGui, QtWidgets
 
 
 def resource_path(relative_path=""):
@@ -55,6 +55,25 @@ class MyMainWindow(MainWindow):
         view_menu.addAction("Clear", self.plotter.clear_actors)
         view_menu.addAction("Scale Axes", self.scale_axes_dialog)
 
+        view_menu.addSeparator()
+        # Orientation marker
+        orien_menu = view_menu.addMenu("Orientation Marker")
+        orien_menu.addAction("Show All", self.plotter.show_axes_all)
+        orien_menu.addAction("Hide All", self.plotter.hide_axes_all)
+        # Bounds axes
+        axes_menu = view_menu.addMenu("Bounds Axes")
+        axes_menu.addAction("Add Bounds Axes (front)", self.plotter.show_bounds)
+        axes_menu.addAction("Add Bounds Grid (back)", self.plotter.show_grid)
+        axes_menu.addAction("Add Bounding Box", self.plotter.add_bounding_box)
+        axes_menu.addSeparator()
+        axes_menu.addAction("Remove Bounding Box", self.plotter.remove_bounding_box)
+        axes_menu.addAction("Remove Bounds", self.plotter.remove_bounds_axes)
+
+        cam_menu = view_menu.addMenu("Camera")
+        self._parallel_projection_action = cam_menu.addAction(
+            "Toggle Parallel Projection", self._toggle_parallel_projection
+        )
+
         # App specific
         self.plotter.main_menu = self.main_menu
 
@@ -65,9 +84,15 @@ class MyMainWindow(MainWindow):
         """Open scale axes dialog."""
         return ScaleAxesDialog(self, self.plotter, show=show)
 
+    def _toggle_parallel_projection(self) -> None:
+        if self.plotter.camera.GetParallelProjection():
+            return self.plotter.disable_parallel_projection()
+        return self.plotter.enable_parallel_projection()
+
 
 if __name__ == "__main__":
     from script import bind_to_plotter
+
     app = QtWidgets.QApplication(sys.argv)
     window = MyMainWindow()
     bind_to_plotter(window.plotter)
